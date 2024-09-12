da9ef8f2f3f43895cf8c53e1707471b3f687643b
JorgeeGlz <l24121310@morelia.tecnm.mx>
2024-09-11 22:43:08 -0600

    tarea3uni2.java

---
+++ tarea3uni2.java
@@ -0,0 +1,42 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//Esta es la sección 1
+System.out.println("Seccion 1");
+System.out.println("Libro:Habitos Atomicos");
+System.out.println("Autor: James Clear");
+System.out.println("Año de publicacion: 2018");
+System.out.println("Genero: Autoayuda");
+System.out.println("Páginas: 336");
+
+//Esta es la seccion 2
+System.out.println("Seccion 2");
+String pais ="Suiza";
+String capital ="Berna";
+String idioma = "Alemán, Francés e Italiano";
+Double poblacion =8.7;
+String moneda = "Fraco suizo";
+System.out.println("País:" + pais);
+System.out.println("Capital:" + capital);
+System.out.println("Idioma:" + idioma);
+System.out.println("Poblacion:" + poblacion);
+System.out.println("Moneda:" + moneda);
+
+//Esta es la seccion 3
+System.out.println("Seccion 3");
+String nombreCompleto = "Diego Armando Maradona";
+String nacimiento = " 30 de octubre de 1960";
+String nacionalidad = "Argentina";
+String ocupacion = "Futbolista";
+String logroDestacado = "Ganador de la copa del Mundo";
+System.out.println("Nombre completo: " + nombreCompleto);
+System.out.println("Nacimiento: " + nacimiento);
+System.out.println("Nacionalidad: " + nacionalidad);
+System.out.println("Ocupacion: " + ocupacion);
+System.out.println("Logro destacado: " + logroDestacado);
+
+
+
+        }
+    }
