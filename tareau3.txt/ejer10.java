2bec16089145a730016d267923d2304c4a9ad902
JorgeeGlz <l24121310@morelia.tecnm.mx>
2024-11-11 22:49:18 -0600

    }ejer10.java

---
+++ tareau3.txt/ejer10.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese el primer número: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Ingrese el segundo número: ");
+        int num2 = scanner.nextInt();
+
+        System.out.print("Ingrese el tercer número: ");
+        int num3 = scanner.nextInt();
+
+        int menor, medio, mayor;
+
+        if (num1 <= num2 && num1 <= num3) {
+            menor = num1;
+            if (num2 <= num3) {
+                medio = num2;
+                mayor = num3;
+            } else {
+                medio = num3;
+                mayor = num2;
+            }
+        } else if (num2 <= num1 && num2 <= num3) {
+            menor = num2;
+            if (num1 <= num3) {
+                medio = num1;
+                mayor = num3;
+            } else {
+                medio = num3;
+                mayor = num1;
+            }
+        } else {
+            menor = num3;
+            if (num1 <= num2) {
+                medio = num1;
+                mayor = num2;
+            } else {
+                medio = num2;
+                mayor = num1;
+            }
+        }
+
+        System.out.println("Números ordenados: " + menor + ", " + medio + ", " + mayor);
+
+        scanner.close();
+    }
+}
