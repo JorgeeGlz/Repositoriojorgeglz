f2b18908f098d7b0e787f7327a0448fc5c7d3ed2
JorgeeGlz <l24121310@morelia.tecnm.mx>
2024-11-11 22:46:38 -0600

    ejer4.java

---
+++ tareau3.txt/ejer4.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese un número: ");
+        int n = scanner.nextInt();
+
+        boolean esPrimo = true;
+
+        if (n <= 1) {
+            esPrimo = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    esPrimo = false;
+                    break;
+                }
+            }
+        }
+
+        if (esPrimo) {
+            System.out.println(n + " es un número primo");
+        } else {
+            System.out.println(n + " no es un número primo");
+        }
+
+        scanner.close();
+    }
+        }
